599f8e5ac3ba6a65b997e655d29bed08776f77d5
mujad <muhamad.alinejad@gmail.com>
2020-11-22 14:45:48 +0330

    rename adbot to conf

--- conf/celery.py
+++ conf/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adbot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
-app = Celery('adbot')
+app = Celery('adtel')
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
