da38c8e0148a5b0976479630219dfb206fca7318
Arash F <arash@hami.blue>
2021-04-04 11:31:47 +0430

    PREV

--- apps/utils/html.py
+++ apps/utils/html.py
@@ -4,7 +4,8 @@ break_line_r = re.compile(r'''<br[^>]*>''')
 
 
 def filter_escape(html):
-    if isinstance(html, str):
-        replaced_breakline = ' \n '.join(list(map(str.strip, break_line_r.split(html))))
-        return replaced_breakline
-    return html
+    if not isinstance(html, str):
+        return html
+
+    replaced_breakline = ' \n '.join(list(map(str.strip, break_line_r.split(html))))
+    return replaced_breakline
